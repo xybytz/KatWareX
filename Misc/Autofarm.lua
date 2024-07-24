826fe9532284995dbf1504c9ecf5c233c0257cd3
xybytz <95513381+xybytz@users.noreply.github.com>
2024-07-24 08:56:45 -0700

    Update Autofarm.lua

--- Misc/Autofarm.lua
+++ Misc/Autofarm.lua
@@ -2,7 +2,7 @@ while true do
 wait(1)
 for i,v in pairs(game.Players:GetChildren()) do
     if v ~= game.Players.LocalPlayer then
-         v.Character.Head.CFrame * CFrame.new(0,0,.4) = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+         v.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,.4) = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
          wait(0.1)
          game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-7, 958, -36)
     end

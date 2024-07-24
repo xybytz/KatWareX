4329e6a16d323b1d6f9fb0c593fe7086faf1283a
xybytz <95513381+xybytz@users.noreply.github.com>
2024-07-24 08:36:07 -0700

    Update Autofarm.lua
    new auto farm method

--- Misc/Autofarm.lua
+++ Misc/Autofarm.lua
@@ -2,7 +2,7 @@ while true do
 wait(1)
 for i,v in pairs(game.Players:GetChildren()) do
     if v ~= game.Players.LocalPlayer then
-         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.Head.CFrame * CFrame.new(0,0,.4)
+         v.Character.Head.CFrame * CFrame.new(0,0,.4) = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
          mouse1click()
          wait(0.1)
          game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-7, 958, -36)
